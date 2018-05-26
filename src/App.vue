7a2d2729b35b969302dd0e2146491befcdbde327
Maksim Masterenko <mmasterenko@gmail.com>
2018-05-26 19:31:30 +0300

    lesson 8: home page

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,13 @@
           @click="drawer = !drawer"
           class="hidden-md-and-up"
         ></v-toolbar-side-icon>
-        <v-toolbar-title>Ad application</v-toolbar-title>
+        <v-toolbar-title>
+          <router-link
+            to="/"
+            tag="span"
+            class="pointer"
+          >Ad application</router-link>
+        </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn
@@ -59,3 +65,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+</style>
