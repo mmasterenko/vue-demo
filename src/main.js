32e7a1572a922ac7906155511d6e6da7094cb858
Maksim Masterenko <mmasterenko@gmail.com>
2018-05-29 14:15:24 +0300

    lesson 15: firebase init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import Vuetify from 'vuetify'
+import * as fb from 'firebase'
+import App from './App'
 import router from './router'
 import store from './store'
 import 'vuetify/dist/vuetify.min.css'
@@ -17,5 +18,15 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyAYBCmmet58fsmlt9BwOh37xBtzNLQj-_I',
+      authDomain: 'itc-ads-20190.firebaseapp.com',
+      databaseURL: 'https://itc-ads-20190.firebaseio.com',
+      projectId: 'itc-ads-20190',
+      storageBucket: 'itc-ads-20190.appspot.com',
+      messagingSenderId: '401547622690'
+    })
+  }
 })
