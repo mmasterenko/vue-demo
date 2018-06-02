a8e79793c7a807d56ee4dff1d5b6801c0f26ab52
Maksim Masterenko <mmasterenko@gmail.com>
2018-06-02 19:10:53 +0300

    lesson 24: edit modal dialog

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <v-card>
+        <v-card v-if="!loading">
           <v-card-media
             :src="ad.imageSrc"
             height="300px"
@@ -13,22 +13,38 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn class="warning" flat>Edit</v-btn>
+            <app-edit-add-modal :ad="ad"></app-edit-add-modal>
             <v-btn class="success">Buy</v-btn>
           </v-card-actions>
         </v-card>
+        <div v-else class="text-xs-center">
+          <v-progress-circular
+            indeterminate
+            :size="100"
+            :width="4"
+            color="purple"
+          ></v-progress-circular>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+  import EditAdModal from './EditAdModal'
+
   export default {
     props: ['id'],
+    components: {
+      appEditAddModal: EditAdModal
+    },
     computed: {
       ad () {
         const id = this.id
         return this.$store.getters.adById(id)
+      },
+      loading () {
+        return this.$store.getters.loading
       }
     }
   }
